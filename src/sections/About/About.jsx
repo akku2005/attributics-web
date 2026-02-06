2840c0dae5b4251c7d7a958782aa2f9907eef3f5
Manaswa S <mnswamain@gmail.com>
2026-02-06 19:14:20 +0530

    feature: about page

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,34 +1,40 @@
 import { siteContent } from '../../constants/content';
-import { mediaMap } from './constants';
+import { getGroupPhotos } from './constants';
 import unionPattern from '../../assets/Union.svg';
+import { Container } from '../../components';
 
 const metricCards = [
     {   
         index: 1,
-        metric: "15%",
+        metric: 15,
+        unit: "%",
         subheadline: "sub-headline",
     },
     {   
         index: 2,
-        metric: "100+",
+        metric: 100,
+        unit: "+",
         subheadline: "sub-headline",
     },
     {   
         index: 3,
-        metric: "750K",
+        metric: 750,
+        unit: "K",
         subheadline: "sub-headline",
     },
     {   
         index: 4,
-        metric: "15%",
+        metric: 15,
+        unit: "%",
         subheadline: "sub-headline",
     }
 ]
 
+
+
+
 const About = () => {
     const { about } = siteContent;
-    const Team1 = mediaMap['team1'];
-    const Team2 = mediaMap['team2'];
 
     return (
         <section id="about" className="py-16 lg:py-24 bg-white mt-10">
@@ -56,16 +62,16 @@ const About = () => {
                         </div>
 
                         {/* Right Image - 473px × 468px */}
-                        <div className="w-[473px] h-[468px]">
+                        <div className="w-[473px] h-[468px] items-end">
                             <div className="w-full h-full bg-[#D9D9D9] rounded-[10px] flex items-center justify-center">
-                                <Team1 className="w-full max-h-full" />
+                                <img src={getGroupPhotos('1')} alt="Group Photo" className="w-full h-full object-cover rounded-[10px]" loading='lazy' />
                             </div>
                         </div>
                     </div>
                 </div>
 
                 {/* Catchy Statement Section */}
-                <div className="relative min-w-[1105px] min-h-[210px] w-full max-w-[1440px] text-center mb-24">
+                <div className="relative w-[1105px] min-h-[210px] w-full max-w-[1440px] text-center mb-24">
                     <h3 className="text-[32px] lg:text-[40px] font-normal leading-[140%] text-black italic max-w-[900px] mx-auto"
                         style={{ fontFamily: 'Noto Sans, sans-serif' }}>
                         "{about.statement}"
@@ -73,16 +79,16 @@ const About = () => {
                 </div>
 
                 {/* Who Are We Section - Group 99: 1113px × 468px */}
-                <div className="relative w-full min-w-[1067px] min-h-[468px] max-w-[1113px]">
+                <div className="relative w-full max-w-[1113px] h-[468px]">
                     {/* Grid Layout for Side-by-Side */}
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-[128px] items-start h-full">
-                        {/* Right Image - 473px × 468px */}
-                        <div className="w-[473px] h-[468px]">
+                        {/* Left Image - 473px × 468px */}
+                        <div className="w-[473px] h-[468px] ">
                             <div className="w-full h-full bg-[#D9D9D9] rounded-[10px] flex items-center justify-center">
-                                <Team2 className="w-full max-h-full" />
+                                <img src={getGroupPhotos('2')} alt="Group Photo" className="w-full h-full object-cover rounded-[10px]" loading='lazy' />
                             </div>
                         </div>
-                        {/* Left Content - 512px wide */}
+                        {/* Right Content - 512px wide */}
                         <div className="w-[512px] flex flex-col items-start gap-[13px] pt-[68px]">
                         <p className="text-[16px] font-normal leading-[130%] uppercase text-black"
                                 style={{ fontFamily: 'IBM Plex Sans, sans-serif' }}>
@@ -102,7 +108,8 @@ const About = () => {
             </div>
 
              {/* Metrics Section */}
-             <div className="mx-auto px-[163px] relative w-full max-w-[1376px] min-h-[600px] lg:h-[539px] overflow-hidden mb-8 lg:mb-12 animate-fade-in-up" style={{ animationDelay: '0.2s' }}>
+             <Container>
+             <div className="mx-auto px-[44px] relative w-full h-[600px] lg:h-[539px] overflow-hidden mb-8 lg:mb-12 animate-fade-in-up" style={{ animationDelay: '0.2s' }}>
                     {/* Dot Pattern Background with Gradient */}
                     <div className="absolute inset-0">
                         {/* Base dot pattern using SVG */}
@@ -110,6 +117,7 @@ const About = () => {
                             src={unionPattern}
                             alt="Dotted Gradient Pattern"
                             className="w-full h-full object-cover"
+                            loading='lazy'
                         />
                         {/* Gradient overlay to create the coral fade effect */}
                         <div
@@ -121,7 +129,7 @@ const About = () => {
                     </div>
 
                     {/* White Card Overlay */}
-                    <div className="absolute min-w-[1289px] min-h-[271px] top-[204px] left-[44px]
+                    <div className="relative w-full h-[271px] top-[204px]
                                     pt-[40px] pr-[50px] pb-[40px] pl-[50px]
                                     border bg-white rounded-[20px] shadow-lg
                                     flex items-center">
@@ -129,7 +137,7 @@ const About = () => {
                             {metricCards.map((card, index) => (
                                 <div
                                     key={index}
-                                    className="min-w-[146px] min-h-[130px] flex flex-col items-center justify-center text-center"
+                                    className="w-[146px] h-[130px] flex flex-col items-center justify-center text-center"
                                 >
                                     {/* Headline */}
                                     <p
@@ -144,7 +152,12 @@ const About = () => {
                                         "
                                     >
                                         {card.metric}
+                                        {card.unit}
                                     </p>
+
+                                    {/* <div className="flex items-end">
+                                          <span className="ml-1 text-[32px] font-mono">{card.unit}</span>
+                                    </div> */}
 
                                     {/* Subheadline */}
                                     <p
@@ -166,6 +179,7 @@ const About = () => {
                         </div>
                     </div>
             </div>
+            </Container>
         
         </section>
     );
